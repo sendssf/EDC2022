f8e38c7582dc4abb9c4900f1d49d7bc40541568f
z-c21 <1624306534@qq.com>
2022-11-25 22:05:09 +0800

    build graph

--- Core/Inc/dijkstra.h
+++ Core/Inc/dijkstra.h
@@ -6,5 +6,19 @@
 
 #define MaxNum 167
 
+typedef struct
+{
+	int order;
+	int info;
+}VertexType;
+ 
+typedef struct
+{
+	uint8_t edges[MaxNum][MaxNum];
+	int n, e;//顶点数，和边数
+	VertexType vex[MaxNum];//存放结点信息
+}Graph;
+
+Position Vnode[MaxNum];
 
 #endif
